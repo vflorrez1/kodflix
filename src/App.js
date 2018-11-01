a90fb38f14132b0d34509a8f66d3547c51b7b9db
Victor <Victor@Katharines-MBP.localdomain>
2018-11-01 14:55:51 +0000

    added 6 images to fit the web page in 2 rows and 3 columns (#1,#2 and #3)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import niceGuysFc from './niceguys.jpg';
+import suitsFc from './suits.jpg';
+import narutoFc from './naruto.jpg';
+import howimetyourmotherFc from './howimetyourmother.jpg';
+import breakingbadFc from './breakingbad.jpg';
+import alteredcarbonFc from './alteredcarbon.jpg';
 
 class App extends Component {
   render() {
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to flix</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="container">
+          <div className="item"><img className="movieImage" src={niceGuysFc} alt="" /></div>
+          <div className="item"><img className="movieImage" src={suitsFc} alt="" /></div> 
+          <div className="item"><img className="movieImage" src={narutoFc} alt="" /></div>
+        </div>
+        <div className="container">
+          <div className="item"><img className="movieImage" src={howimetyourmotherFc} alt="" /></div>
+          <div className="item"><img className="movieImage" src={breakingbadFc} alt="" /></div>
+          <div className="item"><img className="movieImage" src={alteredcarbonFc} alt="" /></div>
+        </div>
+
       </div>
     );
   }
