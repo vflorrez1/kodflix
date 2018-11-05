d39c1bfebe73d51169c4dc0d5d9f1120bc9f1d44
Victor <Victor@Katharines-MBP.localdomain>
2018-11-05 13:13:40 +0000

    componetized the TV show elements for scalability (#7)

--- src/App.js
+++ src/App.js
@@ -7,60 +7,48 @@ import howimetyourmotherFc from './howimetyourmother.jpg';
 import breakingbadFc from './breakingbad.jpg';
 import alteredcarbonFc from './alteredcarbon.jpg';
 
+class FilmBlock extends React.Component {
+  render() {
+    return (
+      <div className="item">
+        <img className="movieImage" src={this.props.srcname} alt="" />
+        <div className="overlay">
+          <div className="text">{this.props.filmname}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
 class App extends Component {
   render() {
 
 
     return (
       <div className="App">
+         <div className="container">
 
-        <div className="container">
-          <div className="item">
-          <img className="movieImage" src={niceGuysFc} alt="" />
-            <div className="overlay">
-              <div className="text">Nice guys</div>
-            </div>
-          </div>
-          <div className="item">
-          <img className="movieImage" src={suitsFc} alt="" />
-          <div className="overlay">
-              <div className="text">Suits</div>
-            </div>
+          <FilmBlock srcname={niceGuysFc} filmname="Nice guys" />
+          <FilmBlock srcname={suitsFc} filmname="Suits" />
+          <FilmBlock srcname={narutoFc} filmname="Naruto" />
+
          </div>
-          <div className="item">
-          <img className="movieImage" src={narutoFc} alt="" />
-          <div className="overlay">
-              <div className="text">Naruto</div>
-            </div>
-          </div>
-        </div>
 
         <div className="container">
-          <div className="item">
-          <img className="movieImage" src={howimetyourmotherFc} alt="" />
-          <div className="overlay">
-              <div className="text">How I met your mother</div>
-            </div>
-          </div>
-          <div className="item">
-          <img className="movieImage" src={breakingbadFc} alt="" />
-          <div className="overlay">
-              <div className="text">Breaking Bad</div>
-            </div>
-          </div>
-          <div className="item">
-          <img className="movieImage" src={alteredcarbonFc} alt="" />
-          <div className="overlay">
-              <div className="text">Altered Carbon</div>
-            </div>
-          </div>
+
+          <FilmBlock srcname={howimetyourmotherFc} filmname="How I met your Mother" />
+          <FilmBlock srcname={breakingbadFc} filmname="Breaking Bad" />
+          <FilmBlock srcname={alteredcarbonFc} filmname="Altere Carbon" />
+
         </div>
       </div>
-     );
-    }
+    );
   }
-          
-          
+}
+
+
 
 
 export default App;
