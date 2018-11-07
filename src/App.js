12714760f537da3c297636a9819b20f4c73beb37
Victor <Victor@Katharines-MBP.localdomain>
2018-11-07 09:48:20 +0000

    added link image which sends you to a new page showing details (#8)

--- src/App.js
+++ src/App.js
@@ -6,49 +6,70 @@ import narutoFc from './naruto.jpg';
 import howimetyourmotherFc from './howimetyourmother.jpg';
 import breakingbadFc from './breakingbad.jpg';
 import alteredcarbonFc from './alteredcarbon.jpg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 class FilmBlock extends React.Component {
   render() {
     return (
       <div className="item">
-        <img className="movieImage" src={this.props.srcname} alt="" />
+
+      < Link to="/deets">
+        
+      <img className="movieImage" src={this.props.srcname} alt="" />
+        
+        </Link>
+        
         <div className="overlay">
-          <div className="text">{this.props.filmname}</div>
+         <div className="text">
+
+         {this.props.filmname}
+
+         </div>
         </div>
       </div>
     );
   }
+}
+
+
+
+function Deets() {
+  return (
+    <h2 className="deetstext"> hey </h2>
+  );
+}
+
+function Gallery() {
+  return (
+    <div>
+      <div className="container">
+        <FilmBlock srcname={niceGuysFc} filmname="Nice Guys" />
+        <FilmBlock srcname={suitsFc} filmname="Suits" />
+        <FilmBlock srcname={narutoFc} filmname="Naruto" />
+      </div>
+      <div className="container">
+        <FilmBlock srcname={howimetyourmotherFc} filmname="How I met your Mother" />
+        <FilmBlock srcname={breakingbadFc} filmname="Breaking Bad" />
+        <FilmBlock srcname={alteredcarbonFc} filmname="Altered Carbon" />
+      </div>
+    </div>
+  );
 }
 
 
 
 class App extends Component {
   render() {
-
-
     return (
       <div className="App">
-         <div className="container">
-
-          <FilmBlock srcname={niceGuysFc} filmname="Nice guys" />
-          <FilmBlock srcname={suitsFc} filmname="Suits" />
-          <FilmBlock srcname={narutoFc} filmname="Naruto" />
-
-         </div>
-
-        <div className="container">
-
-          <FilmBlock srcname={howimetyourmotherFc} filmname="How I met your Mother" />
-          <FilmBlock srcname={breakingbadFc} filmname="Breaking Bad" />
-          <FilmBlock srcname={alteredcarbonFc} filmname="Altere Carbon" />
-
-        </div>
+        <Route exact path="/" component={Gallery} />
+        <Route exact path="/deets" component={Deets} />
       </div>
     );
   }
 }
 
-
-
-
 export default App;
+
+
