e385967575a859f381bd4a77debc53c92eb05420
Victor <Victor@Katharines-MBP.localdomain>
2018-12-05 09:56:04 +0000

    downloaded nodemon for hot reload and added a script (20)

--- src/backend/App.js
+++ src/backend/App.js
@@ -3,6 +3,6 @@ const app = express()
 const port = 3000
 var filmList = require('./filmList')
 
-app.get('/rest/shows', (req, res) => res.send(filmList))
+app.get('/', (req, res) => res.send(filmList))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
